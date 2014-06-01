9c8da295ee634c76c80f22de02dd7532a818a33c
totrit <totrit@gmail.com>
2014-06-02 00:30:40 +0800

    clean up.

--- src/com/totrit/livemonitor/MainActivity.java
+++ src/com/totrit/livemonitor/MainActivity.java
@@ -36,41 +36,8 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Toast;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e. status bar and
- * navigation/system bar) with user interaction.
- * 
- * @see SystemUiHider
- */
 public class MainActivity extends Activity {
   private final static String LOG_TAG = "MainActivity";
-  /**
-   * Whether or not the system UI should be auto-hidden after {@link #AUTO_HIDE_DELAY_MILLIS}
-   * milliseconds.
-   */
-  private static final boolean AUTO_HIDE = true;
-
-  /**
-   * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after user interaction before
-   * hiding the system UI.
-   */
-  private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-  /**
-   * If set, will toggle the system UI visibility upon interaction. Otherwise, will show the system
-   * UI visibility upon interaction.
-   */
-  private static final boolean TOGGLE_ON_CLICK = true;
-
-  /**
-   * The flags to pass to {@link SystemUiHider#getInstance}.
-   */
-  private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-  /**
-   * The instance of the {@link SystemUiHider} for this activity.
-   */
-  private SystemUiHider mSystemUiHider;
 
   /**
    * The main view that the activity contains.
@@ -95,13 +62,10 @@ public class MainActivity extends Activity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    //TODO don't hide the title, because option menu is triggered through the title.
-//    requestWindowFeature(Window.FEATURE_NO_TITLE);
     setContentView(R.layout.activity_main);
     mSurfaceView = (MainView)findViewById(R.id.mainView);
     mDecorationView = (DecorationView)findViewById(R.id.decorationView);
     mUIControlls = new ControlsGroup();
-//    setupHiderFeature();
     mMessenger = new ServiceMessenger();
   }
   
@@ -156,66 +120,6 @@ public class MainActivity extends Activity {
           }
       }
       return super.onKeyDown(keyCode, event);
-  }
-
-  private void setupHiderFeature() {
-    final View controlsView = findViewById(R.id.fullscreen_content_controls);
-
-    // Set up an instance of SystemUiHider to control the system UI for
-    // this activity.
-    // TODO Should replace mSurfaceView with some other low-placed view that
-    // can hide with the system menu bar.
-    mSystemUiHider = SystemUiHider.getInstance(this, mSurfaceView, HIDER_FLAGS);
-    mSystemUiHider.setup();
-    mSystemUiHider.setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-      // Cached values.
-      int mControlsHeight;
-      int mShortAnimTime;
-
-      @Override
-      @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-      public void onVisibilityChange(boolean visible) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-          // If the ViewPropertyAnimator API is available
-          // (Honeycomb MR2 and later), use it to animate the
-          // in-layout UI controls at the bottom of the
-          // screen.
-          if (mControlsHeight == 0) {
-            mControlsHeight = controlsView.getHeight();
-          }
-          if (mShortAnimTime == 0) {
-            mShortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-          }
-          // TODO: uncomment these when has a custom menu-bar.
-          // controlsView
-          // .animate()
-          // .translationY(visible ? 0 : mControlsHeight)
-          // .setDuration(mShortAnimTime);
-        } else {
-          // If the ViewPropertyAnimator APIs aren't
-          // available, simply show or hide the in-layout UI
-          // controls.
-          controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
-        }
-
-        if (visible && AUTO_HIDE) {
-          // Schedule a hide().
-          delayedHide(AUTO_HIDE_DELAY_MILLIS);
-        }
-      }
-    });
-
-    // Set up the user interaction to manually show or hide the system UI.
-    mSurfaceView.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        if (TOGGLE_ON_CLICK) {
-          mSystemUiHider.toggle();
-        } else {
-          mSystemUiHider.show();
-        }
-      }
-    });
   }
 
   private class ServiceMessenger {
@@ -310,16 +214,6 @@ public class MainActivity extends Activity {
     }
   }
 
-  @Override
-  protected void onPostCreate(Bundle savedInstanceState) {
-    super.onPostCreate(savedInstanceState);
-
-    // Trigger the initial hide() shortly after the activity has been
-    // created, to briefly hint to the user that UI controls
-    // are available.
-    delayedHide(100);
-  }
-
   //
   // @Override
   // protected void onStart() {
@@ -345,20 +239,6 @@ public class MainActivity extends Activity {
     mSurfaceView.onConfigurationChanged(newConfig);
   }
 
-  /**
-   * Touch listener to use for in-layout UI controls to delay hiding the system UI. This is to
-   * prevent the jarring behavior of controls going away while interacting with activity UI.
-   */
-  View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-    @Override
-    public boolean onTouch(View view, MotionEvent motionEvent) {
-      if (AUTO_HIDE) {
-        delayedHide(AUTO_HIDE_DELAY_MILLIS);
-      }
-      return false;
-    }
-  };
-
   Handler mPrimeHandler = new Handler() {
     @Override
     public void handleMessage(Message msg) {
@@ -371,22 +251,5 @@ public class MainActivity extends Activity {
       }
     }
   };
-
-  Handler mHideHandler = new Handler();
-  Runnable mHideRunnable = new Runnable() {
-    @Override
-    public void run() {
-      //TODO
-//      mSystemUiHider.hide();
-    }
-  };
-
-  /**
-   * Schedules a call to hide() in [delay] milliseconds, canceling any previously scheduled calls.
-   */
-  private void delayedHide(int delayMillis) {
-    mHideHandler.removeCallbacks(mHideRunnable);
-    mHideHandler.postDelayed(mHideRunnable, delayMillis);
-  }
 
 }
